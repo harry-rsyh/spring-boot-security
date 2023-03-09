2a8605e9e9cb2f4ac57ec340070c064db520ff1d
UdaHarry <harryrahmansyah97@gmail.com>
2023-03-10 01:30:31 +0700

    Redirect After URL, Remember Me, Costumize Logout, Custom logout button

--- src/main/java/com/harry/security/spring/scurity/ApplicationSecurityConfig.java
+++ src/main/java/com/harry/security/spring/scurity/ApplicationSecurityConfig.java
@@ -14,8 +14,12 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import static com.harry.security.spring.scurity.ApplicationUserRole.*;
+
+import java.util.concurrent.TimeUnit;
+
 import static com.harry.security.spring.scurity.ApplicationUserPermission.*;
 
 @Configuration
@@ -41,7 +45,20 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter{
             .authenticated()
             .and()
             .formLogin()
-            .loginPage("/login").permitAll(); // akan diarahkan sesuai Template Controller (menggunakan thymeleaf), jgn lupa di permitAll untuk semua user
+            .loginPage("/login").permitAll() // akan diarahkan sesuai Template Controller (menggunakan thymeleaf), jgn lupa di permitAll untuk semua user
+            .defaultSuccessUrl("/courses", true) // ketika login success maka halaman pertama yang akan diaksess
+            .and()
+            .rememberMe() // default 2 minggu
+                .tokenValiditySeconds((int)TimeUnit.DAYS.toSeconds(21)) // Waktu valid token bisa digunakan
+                .key("somethingsecure") // key token
+            .and()
+            .logout() // Customize Logout
+                .logoutUrl("/logout")
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET")) // Karena kita disable CSRF maka kita bisa pakai GET untuk logout
+                .clearAuthentication(true)
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID", "remember-me") // Jangan Lupa delete cookies yang tidak digunakan
+                .logoutSuccessUrl("/login"); // ketika logout success jangan lupa arahkan kembali ke login url
     }
     
     @Override
