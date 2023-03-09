12b5356a3fb429856e4f0b9f1ab1510f9d76f9b2
UdaHarry <harryrahmansyah97@gmail.com>
2023-03-10 00:15:25 +0700

    Setup role masing-masing url

--- src/main/java/com/harry/security/spring/scurity/ApplicationSecurityConfig.java
+++ src/main/java/com/harry/security/spring/scurity/ApplicationSecurityConfig.java
@@ -3,6 +3,7 @@ package com.harry.security.spring.scurity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,6 +12,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import static com.harry.security.spring.scurity.ApplicationUserRole.*;
+import static com.harry.security.spring.scurity.ApplicationUserPermission.*;
 
 @Configuration
 @EnableWebSecurity
@@ -26,7 +30,14 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+            .csrf().disable() // TODO: Next Learn
             .authorizeRequests()
+            .antMatchers("/","index","/css/*","/js/*").permitAll()
+            .antMatchers("/api/**").hasRole(STUDENT.name()) // Apakah Role nya STUDENT
+            .antMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission()) // Apakah memiliki otoritas COURSE WRITE
+            .antMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission()) // Apakah memiliki otoritas COURSE WRITE
+            .antMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission()) // Apakah memiliki otoritas COURSE WRITE
+            .antMatchers("/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name()) // Jika DIPINDAH URUTANYA keatas Maka antMatcher Method POST, PUT, DELETE akan diabaikan dan Juga karena ini method terakhir yang di check Maka kita tidak perlu memberikan "HttpMethod.GET"
             .anyRequest()
             .authenticated()
             .and()
@@ -39,11 +50,28 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter{
         UserDetails annaSmithUser = User.builder()
             .username("annasmith")
             .password(passwordEncoder.encode("password"))
-            .roles("STUDENT")
+            // .roles(STUDENT.name())
+            .authorities(STUDENT.getGrantedAuthorities())
+            .build();
+
+        UserDetails lindaUser = User.builder()
+            .username("linda")
+            .password(passwordEncoder.encode("password"))
+            // .roles(ADMIN.name())
+            .authorities(ADMIN.getGrantedAuthorities())
+            .build();
+
+        UserDetails tomUser = User.builder()
+            .username("tom")
+            .password(passwordEncoder.encode("password"))
+            // .roles(ADMINTRAINEE.name())
+            .authorities(ADMINTRAINEE.getGrantedAuthorities())
             .build();
         
         return new InMemoryUserDetailsManager(
-            annaSmithUser
+            annaSmithUser,
+            lindaUser,
+            tomUser
         );
     }
 }
--- src/main/java/com/harry/security/spring/student/Student.java
+++ src/main/java/com/harry/security/spring/student/Student.java
@@ -3,19 +3,24 @@ package com.harry.security.spring.student;
 public class Student {
 
     private final Integer studentId;
-    private final String studentNama;
+    private final String studentName;
 
-    public Student(Integer studentId, String studentNama) {
+    public Student(Integer studentId, String studentName) {
         this.studentId = studentId;
-        this.studentNama = studentNama;
+        this.studentName = studentName;
     }
 
     public Integer getStudentId() {
         return studentId;
     }
 
-    public String getStudentNama() {
-        return studentNama;
+    public String getStudentName() {
+        return studentName;
+    }
+
+    @Override
+    public String toString() {
+        return "Student [studentId=" + studentId + ", studentName=" + studentName + "]";
     }
 
 }
